ad9b056a1a3396c644397a47ca20131542eb957f
Karen J Yang <karenjyang@Karens-MBP.attlocal.net>
2022-07-25 11:18:27 -0500

    change lines.py

--- consumers/models/lines.py
+++ consumers/models/lines.py
@@ -19,9 +19,9 @@ class Lines:
 
     def process_message(self, message):
         """Processes a station message"""
-        if "org.chicago.cta.station" in message.topic():
+        if "stations.arrivals.{station_name}" in message.topic():
             value = message.value()
-            if message.topic() == "org.chicago.cta.stations.table.v1":
+            if message.topic() == "station.table":
                 value = json.loads(value)
             if value["line"] == "green":
                 self.green_line.process_message(message)
