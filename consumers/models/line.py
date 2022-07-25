9980107abeade57616736140d832ee3c68cb7bd6
Karen J Yang <karenjyang@Karens-MBP.attlocal.net>
2022-07-25 11:15:28 -0500

    add code

--- consumers/models/line.py
+++ consumers/models/line.py
@@ -65,7 +65,7 @@ class Line:
                 self._handle_station(value)
             except Exception as e:
                 logger.fatal("bad station? %s, %s", value, e)
-        elif 'org.chicago.cta.station.arrivals.{station_name}' in message_topic: # Set the conditional to the arrival topic
+        elif 'station.arrivals.{station_name}' in message_topic: # Set the conditional to the arrival topic
             self._handle_arrival(message)
         elif "TURNSTILE_SUMMARY" in message_topic: # Set the conditional to the KSQL Turnstile Summary Topic
             json_data = json.loads(message.value())
--- consumers/server.py
+++ consumers/server.py
@@ -77,7 +77,7 @@ def run_server():
             is_avro=False,
         ),
         KafkaConsumer(
-            "station.arrivals.",
+            "station.arrivals.{station_name}",
             lines.process_message,
             offset_earliest=True,
         ),
